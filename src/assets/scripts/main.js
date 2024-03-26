1cbcf1c6ff60f0d0b7a2a7d744667e6006575997
ggiovannyciuoc <ggiovannyci@uoc.edu>
2024-03-26 09:34:06 +0100

    Sass function working

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,24 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+/* Responsive Navigation */
+
+
+const toggleButton = document.querySelector('.header__toggle-menu');
+const menu = document.querySelector('.header__menu');
+
+toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+// Close the menu when a menu item is clicked
+const menuItems = document.querySelectorAll('.header__menu a');
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
+
+
+
